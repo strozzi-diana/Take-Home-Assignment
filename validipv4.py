6cefb5989a44ce8486b650f264607287c53f6b3d
strozzi-diana <71243168+strozzi-diana@users.noreply.github.com>
2020-10-22 22:46:51 -0400

    Add files via upload

---
+++ validipv4.py
@@ -0,0 +1,33 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from keyboard import press
+import logging
+
+
+class MyTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.driver = webdriver.Remote(
+            command_executor = 'http://127.0.0.1:4545/wd/hub',
+            desired_capabilities = {
+            'browserName': 'firefox',
+            'javascriptEnabled': True
+            })
+        
+    def test_ipv4(self):
+        driver = self.driver
+        driver.get('https://censys.io/ipv4')
+        search = driver.find_element_by_id("q")
+        search.send_keys("176.49.198.99")
+        press('enter')
+
+    def tearDown(self):
+        self.driver.close()
+
+logging.basicConfig(filename='standout.log', level=logging.DEBUG,
+                    format='%(asctime)s|%(filename)s|%(message)s|%(pathname)s')
+
+if __name__ == "__main__":
+    unittest.main()
